3186d2385003ab7653a6b0e177733ebe22044f17
Talha Seraj <talhaseraj92@gmail.com>
2022-12-01 16:01:46 +0400

    update

--- src/index.js
+++ src/index.js
@@ -165,8 +165,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Real-Time Object Detection: Kangaroo</h1>
-        <h3>MobileNetV2</h3>
+        <h1>Real-Time Object Detection: Car Side</h1>
+        <h3>Car Side Guided</h3>
         <video
           style={{height: '600px', width: "500px"}}
           className="size"
