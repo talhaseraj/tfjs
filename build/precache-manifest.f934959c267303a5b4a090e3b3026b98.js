4d856f0bc64c66fb2953ba3a003d09f275a7c202
Talha Seraj <talhaseraj92@gmail.com>
2022-12-01 10:53:57 +0400

    first

---
+++ build/precache-manifest.f934959c267303a5b4a090e3b3026b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99a41659bc01215e249e",
+    "url": "/static/css/main.1473bf15.chunk.css"
+  },
+  {
+    "revision": "99a41659bc01215e249e",
+    "url": "/static/js/main.99a41659.chunk.js"
+  },
+  {
+    "revision": "774403264bb090b8e036",
+    "url": "/static/js/1.77440326.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32066842f066556a33bc849ba9d953f0",
+    "url": "/index.html"
+  }
+];
